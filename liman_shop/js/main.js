781504e62ff15c8d86c31d80b544998bb18188ca
webcoder-dev <romochka.tolmachev14@mail.ru>
2019-08-24 18:35:30 +0500

    upd

---
+++ liman_shop/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	// var scroll = $(window).scrollTop();
+	
+	// if (scroll >= 120) {
+	// 	$(".header-page").addClass("header-page__scrolled");
+	// } else {
+	// 	$(".header-page").removeClass("header-page__scrolled");
+	// }
+
+	// $(window).scroll(function() {    
+	// 	var scroll = $(window).scrollTop();
+	
+	// 	if (scroll >= 120) {
+	// 	  $(".header-page").addClass("header-page__scrolled");
+	// 	} else {
+	// 	  $(".header-page").removeClass("header-page__scrolled");
+	// 	}
+	// });
+
+	// $('.scroll').click( function() {
+	// 	var scroll_el = $(this).attr('href');
+	// 	if ($(scroll_el).length != 0) {
+	// 		$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 700);
+	// 	}
+		
+	// 	return false;
+	// });
+});
